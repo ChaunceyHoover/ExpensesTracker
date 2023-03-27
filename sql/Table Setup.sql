393a9a5fbad96bd5c790ff61293d5183c0e571cc
Joshua Campbell <joshuawcampb@gmail.com>
2023-03-26 21:28:35 -0400

    Added Payments View
    Added view to join payments, payees, and payment type.

--- sql/Table Setup.sql
+++ sql/Table Setup.sql
@@ -106,7 +106,7 @@ CREATE TABLE payments (
 
 -- VIEWS
 
-DROP VIEW IF EXISTS dynamic_expenses_view, loans_view;
+DROP VIEW IF EXISTS dynamic_expenses_view, loans_view, payments_view;
 
 CREATE VIEW dynamic_expenses_view
 AS
@@ -124,3 +124,12 @@ AS
     FROM loans lo
     LEFT JOIN payees p
     ON lo.payee_id = p.payee_id;
+    
+CREATE VIEW payments_view
+AS
+    SELECT pmnt.payment_id, pmnt.payment_name, p.payee_id, p.payee_name, pmnt.`date`, pmnt.amount, pmnt.notes
+    FROM payments pmnt
+    LEFT JOIN payees p
+    ON pmnt.payee_id = p.payee_id
+    LEFT JOIN payment_type pt
+    ON pmnt.pt_id = pt.pt_id;
