5e0faf52bae96f57a1ae42e4310e6ef8a35bb12e
Chauncey Hoover <dealernextdoor@gmail.com>
2023-03-28 01:47:52 -0400

    Added model for reoccurring expenses
    This may have been committed out of order, but added model to represent reoccurring expenses that will be applied every billing cycle, which can be specified in the expense.
    
    Examples of these types of expenses could be utilities (start of month to end billing date, with notes saying what date the billing cycle was from) to renter's insurance (start of year to end)

---
+++ src/Models/Mapper/StaticExpenseMapper.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.FluentMap.Mapping;
+
+namespace ExpensesApp.Models.Mapper {
+    public class StaticExpenseMapper : EntityMap<StaticExpense> {
+        public StaticExpenseMapper() {
+            Map(se => se.Id).ToColumn("se_id");
+            Map(se => se.Name).ToColumn("se_name");
+            Map(se => se.StartDate).ToColumn("start_date");
+            Map(se => se.EndDate).ToColumn("end_date");
+            Map(se => se.Amount).ToColumn("amount");
+            Map(se => se.Notes).ToColumn("notes");
+        }
+    }
+}
---
+++ src/Models/StaticExpense.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpensesApp.Models {
+    public class StaticExpense {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public float Amount { get; set; }
+        public string Notes { get; set; }
+    }
+}
