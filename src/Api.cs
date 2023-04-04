102a0f33247247077fd89ccb0f12e583d12c5393
Chauncey Hoover <dealernextdoor@gmail.com>
2023-04-04 15:44:02 -0400

    Switched index to API
    Index page now uses API route to get data instead of pre-populating the page with the value

--- src/Api.cs
+++ src/Api.cs
@@ -12,6 +12,34 @@ namespace ExpensesApp {
         public static void ConfigureApi(this WebApplication app) {
             _config = app.Configuration;
             _logger = app.Logger;
+
+            var group = app.MapGroup("/api");
+
+            group.MapGet("/exp/{id:int}/{start:datetime}/{end:datetime}", GetBalance);
+            group.MapGet("/exp/{id:int}", (int id) => GetBalance(id, DateTime.MinValue, DateTime.MaxValue));
+        }
+
+        private static async Task<IResult> GetBalance(int id, DateTime start, DateTime end) {
+            _logger.LogInformation($"Hit [id:{id}] [start:{start.ToShortDateString()}] [end:{end.ToShortDateString()}]");
+
+            // Get dynamic expenses
+            var expensesSql = @"CALL GetBalances(@payee_id, @start_date, @end_date)";
+
+            try {
+                using (var conn = new MySqlConnection(_config.GetConnectionString("Default"))) {
+                    var expensesResult = await conn.QueryAsync<Expenses, Payee, Expenses>(
+                        expensesSql,
+                        (exp, payee) => { exp.Payee = payee; return exp; },
+                        new { payee_id = id, start_date = start.ToString("yyyy-MM-dd"), end_date = end.ToString("yyyy-MM-dd") },
+                        splitOn: "payee_id"
+                    );
+
+                    return Results.Ok(expensesResult.FirstOrDefault());
+                }
+            } catch(Exception ex) {
+                _logger.LogError($"{ex.Message}\n{ex.StackTrace}");
+                return Results.Problem("Unable to query database");
+            }
         }
     }
 }
---
+++ src/wwwroot/js/index.js
@@ -0,0 +1,33 @@
+﻿// Create our number formatter.
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+function getUserExpenses(id) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            method: 'GET',
+            url: `/api/exp/${id}`,
+
+            success: resolve,
+            error: reject
+        });
+    });
+}
+
+getUserExpenses(1)
+    .then(data => {
+        document.getElementById('user1').innerHTML = formatter.format(data.balance);
+    })
+    .catch((xhr, status, err) => {
+        document.getElementById('user1').innerHTML = '(Unable to get balance)';
+    });
+
+getUserExpenses(2)
+    .then(data => {
+        document.getElementById('user2').innerHTML = formatter.format(data.balance);
+    })
+    .catch((xhr, status, err) => {
+        document.getElementById('user2').innerHTML = '(Unable to get balance)';
+    });
