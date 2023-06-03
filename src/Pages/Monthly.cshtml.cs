31494d734630aa50be32db8e06c7faf7573d4d2a
Chauncey Hoover <dealernextdoor@gmail.com>
2023-06-03 15:53:26 -0400

    Removed unused code
    Now that tables are populated via API calls, removed the unused code that pre-populated the tables with content

--- src/Pages/Monthly.cshtml.cs
+++ src/Pages/Monthly.cshtml.cs
@@ -1,28 +1,6 @@
-using Dapper;
-using ExpensesApp.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using MySqlConnector;
 
 namespace ExpensesApp.Pages
 {
-    public class MonthlyModel : PageModel
-    {
-        private MySqlConnection _conn;
-
-        public IList<StaticExpense> StaticExpenses { get; set; }
-
-        public MonthlyModel([FromServices] MySqlConnection connection)
-            => _conn = connection;
-
-        public async Task OnGetAsync()
-        {
-            // Get static expenses
-            var staticSql = @"SELECT * FROM static_expenses";
-            var staticResults = await _conn.QueryAsync<StaticExpense>(staticSql);
-
-            // Map to results for razor page to access
-            StaticExpenses = staticResults.ToList();
-        }
-    }
+    public class MonthlyModel : PageModel {}
 }
